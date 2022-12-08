d766882c7819144de231d27a0d071174a67de972
Andrey O <opexandrey@gmail.com>
2022-12-08 16:23:30 -0500

    1st commit

---
+++ src/test/java/runners/FailedScenariosRunner.java
@@ -0,0 +1,21 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+
+        features = "@target/failed.txt",
+        glue = "stepDefs",
+        stepNotifications = true
+
+)
+
+
+public class FailedScenariosRunner {
+
+}
---
+++ src/test/resources/test.feature
@@ -0,0 +1,11 @@
+Feature: feature
+
+
+  @temp
+  Scenario: scenario 01
+    Given
+    When
+    Then
+
+
+
